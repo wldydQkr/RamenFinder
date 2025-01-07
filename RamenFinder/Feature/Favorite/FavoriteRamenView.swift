332ef17176be4a7756a9bbc5c1922e5e6f61fe69
wldydQkr <wlqkr1010@gmail.com>
2025-01-07 19:27:17 +0900

    init

---
+++ RamenFinder/Feature/Favorite/FavoriteRamenView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteRamenView.swift
+//  RamenFinder
+//
+//  Created by 박지용 on 1/7/25.
+//
+
+import SwiftUI
+
+struct FavoriteRamenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteRamenView()
+}
